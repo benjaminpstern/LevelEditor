43a192f3c1faae52503e957dd1a4b4d43fa94707
benjaminpstern <bstern@oberlin.edu>
2014-11-22 03:57:53 -0500

    Made patrolling enemies work

---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Point : MonoBehaviour {
+	public Drag parent;
+	private GameObject spawn;
+	private LevelBuildGui script;
+	void Start(){
+		spawn = GameObject.FindWithTag ("GameController");
+		script = spawn.GetComponent<LevelBuildGui> ();
+		parent = script.selectedObject;
+	}
+}
